735b70b36b6fbe77b6d57e01a4ef86b9c520f724
matui3 <jonformantes@gmail.com>
2023-06-27 00:05:25 -0700

    Chess w/ Checkers to help learn

--- Board.py
+++ Board.py
@@ -1,12 +1,18 @@
-import numpy as np
+import Square
 
 class Board:
 
     # this will represent configation of the board aka where pieces are
     # might do this on console first
 
-    def __init__(self):
-        self.board = np.array([[' ' for col in range(8)] for row in range(8)])
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tile_width = width // 8
+        self.tile_height = height // 8
+        # I will create a surface with the initialization of board
+
+
 
     def display_board(self):
-        print(self.board)
+        pass
--- Square.py
+++ Square.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Square:
     
 
@@ -6,8 +8,28 @@ class Square:
         self.y = y
         self.width = width
         self.height = height
+        self.board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP' ,'bP', 'bP', 'bP', 'bP'],
+            ['','','','','','','',''],
+            ['','','','','','','','']
+            ['','','','','','','','']
+            ['','','','','','','','']
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+        ]
+
     
     def get_coordinates(self):
         file = 'ABCDEFGH'
+    
+
+    def draw(self):
+        rect_surf = pygame.Surface((100, 100))
+        rect_surf.fill('red')
+        rectangle = rect_surf.get_rect()
         
+        
+        # pygame.draw.rect()
+        # pygame
 
